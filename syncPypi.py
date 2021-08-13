db20f7e9421a128dab9f38f5c99eb8deeabc543d
endsley <chieh.t.wu@gmail.com>
2021-08-13 15:33:34 -0400

    not important enough to comment

--- syncPypi.py
+++ syncPypi.py
@@ -6,6 +6,6 @@ import time
 os.system('rm -r dist/')
 os.system('python setup.py sdist')
 os.system('twine upload dist/*')
-time.sleep(2)
-os.system('pip install wuml -U')
+#time.sleep(2)
+#os.system('pip install wuml -U')
 
