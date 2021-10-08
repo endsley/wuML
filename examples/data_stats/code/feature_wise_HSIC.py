7e15b5d3f7c899564f64903778d6466aef033d7e
endsley <chieh.t.wu@gmail.com>
2021-10-08 13:59:00 -0400

    not important enough to comment

--- examples/data_stats/code/feature_wise_HSIC.py
+++ examples/data_stats/code/feature_wise_HSIC.py
@@ -19,7 +19,6 @@ print(wuml.feature_wise_HSIC(data, label_name='label', get_top_dependent_pairs=T
 # This function handles missing data as well by removing missing entries during pairwise HSIC
 data = wuml.wData('../../data/missin_example.csv', row_id_with_label=0)
 print(data)
-
+print('\n\n')
 print(wuml.feature_wise_HSIC(data))
-print(wuml.feature_wise_HSIC(data, get_top_dependent_pairs=True))
 
