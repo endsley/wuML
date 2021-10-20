f39ddff70c0a1815fee4738c9b923606019fae06
endsley <chieh.t.wu@gmail.com>
2021-10-20 15:48:36 -0400

    not important enough to comment

--- wuml/preprocess.py
+++ wuml/preprocess.py
@@ -320,7 +320,7 @@ def use_cdf_to_map_data_between_0_and_1(data, output_type_name='wData'):
 
 	output = ensure_data_type(df, type_name=output_type_name)
 	if output_type_name=='wData': 
-		output.Y = data.Y
+		if data.Y not None: output.Y = data.Y
 		output.label_column_name = data.label_column_name
 
 	return output
