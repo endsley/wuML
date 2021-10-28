f962d83a2044e8c5d542eb47771d788d8348e468
endsley <chieh.t.wu@gmail.com>
2021-10-28 11:50:15 -0400

    not important enough to comment

--- examples/regression/code/run_regression.py
+++ examples/regression/code/run_regression.py
@@ -5,11 +5,11 @@ import wuml
 
 data = wuml.wData('../../data/shap_regress_example_uniform.csv', label_column_name='label', label_type='continuous', first_row_is_label=True)
 
-reg = wuml.regression(data, regressor='AdaBoost')
+reg = wuml.regression(data, regressor='Elastic net', alpha=0.05, gamma=0.05, l1_ratio=0.05)
 print('Running a single regressor')
 print(reg)
 
 print('\n\nRun all regressors sorted by least test error')
-result = wuml.run_every_regressor(data)
+result = wuml.run_every_regressor(data, alpha=0.1, gamma=0.05, l1_ratio=0.05)
 print(result)
 
