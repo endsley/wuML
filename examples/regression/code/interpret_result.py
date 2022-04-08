0464b8d7f262bfcf347ffc3aa70b3bb1e3b6b6c0
endsley <chieh.t.wu@gmail.com>
2022-04-08 14:48:15 -0400

    not important enough to comment

--- examples/regression/code/interpret_result.py
+++ examples/regression/code/interpret_result.py
@@ -10,26 +10,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import wplotlib
 
-#How to Interpret feature importance after linear models
-## 5x₁ + x₂ + x₁x₂ - 8x₄ - 2x₄x₄ + δ
+#How to Interpret feature importance after certain models
+wuml.jupyter_print('A non-linear model was used to generate the data. ')
+wuml.jupyter_print('Note that x1 x4 has the largest impact')
+wuml.jupyter_print('5x_1 + x_2 + x_1 x_2 - 8x_4 - 2x_4^2 + \delta', latex=True)
+wuml.jupyter_print('\delta \leftarrow noise', latex=True)
+
 data = wuml.wData('../../data/shap_regress_example_uniform.csv', label_column_name='label', label_type='continuous', first_row_is_label=True)
 cnames = data.get_column_names_as_a_list()
 
 
-#reg = wuml.regression(data, regressor='linear', alpha=0.05, gamma=0.05, l1_ratio=0.05)
-#reg.plot_feature_importance('Feature Importance for Linear Regression', cnames)
-#
-#
-#reg2 = wuml.regression(data, regressor='Elastic net', alpha=0.05, gamma=0.05, l1_ratio=0.05)
-#reg2.plot_feature_importance('Feature Importance for Elastic Net', cnames)
-#
-#
-#reg3 = wuml.regression(data, regressor='Lasso', alpha=0.05)
-#reg3.plot_feature_importance('Feature Importance for Lasso', cnames)
-#
-#
-#reg4 = wuml.regression(data, regressor='Lasso', alpha=0.05)
-#reg4.plot_feature_importance('Feature Importance for Lasso', cnames)
+reg = wuml.regression(data, regressor='linear', alpha=0.05, gamma=0.05, l1_ratio=0.05)
+reg.plot_feature_importance('Feature Importance for Linear Regression', cnames)
+
+
+reg2 = wuml.regression(data, regressor='Elastic net', alpha=0.05, gamma=0.05, l1_ratio=0.05)
+reg2.plot_feature_importance('Feature Importance for Elastic Net', cnames)
+
+
+reg3 = wuml.regression(data, regressor='Lasso', alpha=0.05)
+reg3.plot_feature_importance('Feature Importance for Lasso', cnames)
+
+
+reg4 = wuml.regression(data, regressor='Lasso', alpha=0.05)
+reg4.plot_feature_importance('Feature Importance for Lasso', cnames)
 
 
 reg5 = wuml.regression(data, regressor='RandomForest')
