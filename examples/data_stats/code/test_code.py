b26fdabf74f2afdf1fd28ec2c2fdeb43f6cbdd7b
endsley <chieh.t.wu@gmail.com>
2023-02-10 08:59:46 -0500

    not important enough to comment

--- examples/data_stats/code/test_code.py
+++ examples/data_stats/code/test_code.py
@@ -10,9 +10,20 @@ code_list.append('get_stats_on_data_with_missing_entries.py')
 code_list.append('show_feature_histogram.py')
 code_list.append('weight_sample_by_rarity.py')
 
-print('Running data_stats folder')
+#print('Running data_stats folder')
+#for code in code_list:
+#	print('\tRunning ' + code)
+#	os.system('./' + code + ' disabled')
 
+print('Generate ipynb files')
 for code in code_list:
-	print('\tRunning ' + code)
-	os.system('./' + code + ' disabled')
+	print('\tGenerating ' + code)
+	os.system('p2j -o ' + code)
+	ipynb = code.replace('.py', '.ipynb')
+	os.system('jupyter nbconvert --to notebook --execute ' + ipynb + ' > /dev/null 2>&1')
+
+	ran_ipynb = ipynb.replace('.ipynb', '.nbconvert.ipynb')
+	os.system('mv ' + ran_ipynb + ' ' + ipynb)
+	os.system('mv ' + ipynb + ' ../ipynb/' + ipynb)
+	
 
